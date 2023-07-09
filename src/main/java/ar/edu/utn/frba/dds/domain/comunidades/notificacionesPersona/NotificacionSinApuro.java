be89068713dc5cdd29a2cd2f24142aeeca81c6c8
_marcos <metorres89@gmail.com>
2023-07-09 19:17:52 -0300

    deteccion de incidentes cercanos y notificaciones sin apuro
    - agrego deteccion de incidentes cercanos
    - cambios en notificaciones sin apuro

--- src/main/java/ar/edu/utn/frba/dds/domain/comunidades/notificacionesPersona/NotificacionSinApuro.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/comunidades/notificacionesPersona/NotificacionSinApuro.java
@@ -3,16 +3,43 @@ package ar.edu.utn.frba.dds.domain.comunidades.notificacionesPersona;
 import ar.edu.utn.frba.dds.domain.comunidades.Persona;
 import ar.edu.utn.frba.dds.notificaciones.Notificable;
 
+import ar.edu.utn.frba.dds.notificaciones.Notificador;
 import java.time.DayOfWeek;
+import java.time.LocalTime;
+import java.util.Dictionary;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import lombok.Getter;
+import lombok.Setter;
 
 public class NotificacionSinApuro implements EstrategiaMomentoNotificacion {
+    @Getter
     private ListadoNotificables notificablesSinNotificar;
-    private Map<DayOfWeek, List<Horario>> fechas;
+
+    @Getter
+    private Map<DayOfWeek, List<LocalTime>> fechas;
+
+    @Setter
+    private Persona persona;
+
+    public NotificacionSinApuro() {
+        this.notificablesSinNotificar = new ListadoNotificables();
+        this.fechas = new HashMap<DayOfWeek, List<LocalTime>>();
+    }
 
     @Override
     public void notificar(Notificable notificable, Persona persona) {
+        if(this.persona == null) {
+            this.persona = persona;
+        }
+        this.notificablesSinNotificar.agregarNotificables(notificable);
+    }
 
+    public void enviarNotificaciones() {
+        if(!this.notificablesSinNotificar.estaVacio()) {
+            Notificador.notificar(this.notificablesSinNotificar, this.persona);
+            this.notificablesSinNotificar.vaciarNotificables();
+        }
     }
 }
