7ded2e3654b0b6d227318136b1c75a3d9020573b
Mateo Bertogliati <mbertogliati@frba.utn.edu.ar>
2023-07-09 20:21:18 -0300

    Agrego observadores

--- src/main/java/ar/edu/utn/frba/dds/domain/comunidades/Membresia.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/comunidades/Membresia.java
@@ -1,7 +1,11 @@
 package ar.edu.utn.frba.dds.domain.comunidades;
 
+import ar.edu.utn.frba.dds.domain.servicios.Servicio;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Membresia {
   @Getter
@@ -10,11 +14,17 @@ public class Membresia {
   private Comunidad comunidad;
   @Getter @Setter
   private Rol rolComunidad;
-  @Getter @Setter
-  private boolean afectado;
+  @Getter
+  private List<Servicio> serviciosObservados;
 
   public Membresia(Comunidad comunidad, Persona persona){
     this.persona = persona;
     this.comunidad = comunidad;
+    serviciosObservados = new ArrayList<>();
   }
+
+  public boolean estaAfectado(Servicio servicio){
+    return !serviciosObservados.contains(servicio);
+  }
+
 }
