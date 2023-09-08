e230a8b6449afc436dbdb2f385cba95b90157c64
mcotens <mcotens@frba.utn.edu.ar>
2023-09-08 01:53:36 -0300

    Update PropuestaFusion.java

--- service-project/src/main/java/ar/edu/utn/frba/domain/entidades/PropuestaFusion.java
+++ service-project/src/main/java/ar/edu/utn/frba/domain/entidades/PropuestaFusion.java
@@ -6,20 +6,8 @@ import lombok.Setter;
 @Getter
 @Setter
 public class PropuestaFusion {
-    private static int contadorId = -1;
-
-    private int id;
     private Long idOrganizacion1;
     private Long idOrganizacion2;
 
     private Organizacion organizacionFusionada;
-
-    public PropuestaFusion(){
-        this.id = this.nextId();
-    }
-
-    public int nextId() {
-        contadorId++;
-        return contadorId;
-    }
 }
