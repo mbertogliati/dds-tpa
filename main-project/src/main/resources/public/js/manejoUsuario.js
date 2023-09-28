c8445c3f79b70f3a46ae0a4f5dd672a38f6d87f5
Mateo Bertogliati <mbertogliati@frba.utn.edu.ar>
2023-09-28 10:37:21 -0300

    Fix editar usuario: - Se muestra/esconde correctamente las fechas segun el tipo de notificacion - Se eliminan loops for innecesarios - Se previene el autocompletado del new password

--- main-project/src/main/resources/public/js/manejoUsuario.js
+++ main-project/src/main/resources/public/js/manejoUsuario.js
@@ -1,17 +1,14 @@
 //CAMBIAR A NUEVO ARCHIVO JAVASCRIPT
 function mostrarEsconderFechasDeSemana(){
-    var fechasDeSemana = document.getElementById("fechas_de_semana");
-    if(this.getAttribute("value") == "sinApuro"){
-        fechasDeSemana.style.visibility = "visible";
+    var fechasDeSemana = document.getElementById("fechas_de_semana_container");
+    if(document.getElementById("momento_notificacion").value === "alMomento"){
+        fechasDeSemana.style.display = "none";
         return;
     }
-    fechasDeSemana.style.visibility = "hidden";
+    fechasDeSemana.style.display = "block";
 }
-function agregarEvento(){
-
-}
-
 document.addEventListener("DOMContentLoaded", function() {
+
     document.getElementById("momento_notificacion").addEventListener("change",mostrarEsconderFechasDeSemana);
 
     const selectMedio = document.getElementById("medio_notificacion");
@@ -19,30 +16,13 @@ document.addEventListener("DOMContentLoaded", function() {
     const selectMomento = document.getElementById("momento_notificacion");
     const momentoNotificacion = selectMomento.getAttribute("data-momento");
 
-    for (let i = 0; i < selectMedio.options.length; i++) {
-        if (selectMedio.options[i].value === medioNotificacion) {
-            selectMedio.options[i].selected = true;
-            break;
-        }
-    }
-
-    for (let i = 0; i < selectMomento.options.length; i++) {
-        if (selectMomento.options[i].value === momentoNotificacion) {
-            selectMomento.options[i].selected = true;
-            break;
-        }
-    }
+    selectMedio.value = medioNotificacion
+    selectMomento.value = momentoNotificacion
 
     const selectsDias = document.getElementsByClassName("dia");
     for (let i = 0; i < selectsDias.length; i++){
         const selectDia = selectsDias[i];
-        const valorDia = selectDia.getAttribute("data-dia");
-        for (let j = 0; j < selectDia.options.length; j++) {
-            if (selectDia.options[j].value === valorDia) {
-                selectDia.options[j].selected = true;
-                break;
-            }
-        }
+        selectDia.value = selectDia.getAttribute("data-dia");
     }
 
     const inputsTime = document.getElementsByClassName("time-notificacion");
@@ -51,4 +31,5 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let i = 0; i < inputsTime.length; i++){
         inputsTime[i].step = stepEnSegundos.toString();
     }
+    mostrarEsconderFechasDeSemana();
 });
