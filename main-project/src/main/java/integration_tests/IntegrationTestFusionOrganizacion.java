74f2d0d2405629936231aa2793fb4d04c9139fbb
_marcos <metorres89@gmail.com>
2023-10-11 10:57:47 -0300

    Agrego código para probar la integración con aceptarPropuesta
    Agrego código para probar la integración con aceptarPropuesta

--- main-project/src/main/java/integration_tests/IntegrationTestFusionOrganizacion.java
+++ main-project/src/main/java/integration_tests/IntegrationTestFusionOrganizacion.java
@@ -1,8 +1,10 @@
 package integration_tests;
 
 import ar.edu.utn.frba.dds.modelos.fusion_organizacion.AdapterFusion;
+import ar.edu.utn.frba.dds.modelos.fusion_organizacion.FusionCompletada;
 import ar.edu.utn.frba.dds.modelos.fusion_organizacion.Organizacion;
 import ar.edu.utn.frba.dds.modelos.fusion_organizacion.PropuestaFusion;
+import ar.edu.utn.frba.dds.modelos.fusion_organizacion.SolicitudFusion;
 import ar.edu.utn.frba.dds.modelos.fusion_organizacion.servicio_externo.ServicioFusion;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +14,7 @@ public class IntegrationTestFusionOrganizacion {
   public static void main(String args[]) {
     AdapterFusion adapterFusion = new ServicioFusion();
     testObtenerPropuestas(adapterFusion);
+    testAceptarPropuestas(adapterFusion);
   }
 
   private static void testObtenerPropuestas(AdapterFusion adapterFusion) {
@@ -20,11 +23,44 @@ public class IntegrationTestFusionOrganizacion {
     Organizacion organizacionB = obtenerOrganizacionTest(2l);
     organizaciones.add(organizacionA);
     organizaciones.add(organizacionB);
+
+    System.out.println("Obteniendo propuestas de fusión...");
+
     List<PropuestaFusion> propuestas = adapterFusion.obtenerPropuestas(organizaciones);
 
+    if(propuestas == null) {
+      System.out.println("No se encontraron propuestas!");
+      return;
+    }
+
+    System.out.println("Se encontraron estas propuestas de fusión:");
+
     propuestas.forEach(p -> {
-      System.out.println("IdOrganizacion1:" + p.getIdOrganizacion1() + " - IdOrganizacion2:" + p.getIdOrganizacion2());
+      System.out.println("\tPropuesta:  IdOrganizacion1:" + p.getIdOrganizacion1() + " - IdOrganizacion2:" + p.getIdOrganizacion2());
     });
+  }
+
+  private static void testAceptarPropuestas(AdapterFusion adapterFusion) {
+    SolicitudFusion solicitudFusion = new SolicitudFusion();
+
+    solicitudFusion.setOrganizacion1(obtenerOrganizacionTest(1l));
+    solicitudFusion.setOrganizacion2(obtenerOrganizacionTest(2l));
+
+    System.out.println("Aceptando fusión...");
+
+    FusionCompletada fusionCompletada = adapterFusion.aceptarFusion(solicitudFusion);
+
+    if(fusionCompletada == null) {
+      System.out.println("No se pudo realizar la fusión!");
+      return;
+    }
+
+    System.out.println("Fusion completada entre organizaciones: idOrganizacion1: " + fusionCompletada.getIdOrganizacion1() + ", idOrganizacion2: " + fusionCompletada.getIdOrganizacion2());
+
+    Organizacion nuevaOrganizacion = fusionCompletada.getOrganizacionFusionada();
+
+    System.out.println("Nueva organizacion generada");
+    mostrarOrganizacion(nuevaOrganizacion);
   }
 
   private static Organizacion obtenerOrganizacionTest(Long idOrganizacion) {
@@ -38,5 +74,13 @@ public class IntegrationTestFusionOrganizacion {
     organizacion.getEstablecimientos().add(2l);
     organizacion.setGradoConfianza(1.0d);
     return organizacion;
+  }
+
+  private static void mostrarOrganizacion(Organizacion organizacion) {
+    System.out.println("\tid: "  + organizacion.getIdOrganizacion());
+    System.out.println("\tgradoConfianza: "  + organizacion.getGradoConfianza());
+    System.out.println("\tmiembros:" + organizacion.getMiembros());
+    System.out.println("\tservicios:" + organizacion.getServicios());
+    System.out.println("\testablecimientos:" + organizacion.getEstablecimientos());
   }
 }
