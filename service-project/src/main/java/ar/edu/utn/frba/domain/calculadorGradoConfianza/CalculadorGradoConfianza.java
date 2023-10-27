9df971bb80eea0dc699ed089977286404f955812
_marcos <metorres89@gmail.com>
2023-10-27 15:24:42 -0300

    Cambios en proyecto para subir a la nube
    - Borro submodulo de servicio-1
    - Agrego propiedades adicionales en pom

--- service-project/src/main/java/ar/edu/utn/frba/domain/calculadorGradoConfianza/CalculadorGradoConfianza.java
+++
@@ -1,7 +0,0 @@
-package ar.edu.utn.frba.domain.calculadorGradoConfianza;
-
-import ar.edu.utn.frba.domain.entidades.Organizacion;
-
-public interface CalculadorGradoConfianza {
-    Double calcularGradoConfianza(Organizacion org1, Organizacion org2);
-}
